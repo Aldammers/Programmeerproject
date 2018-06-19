98ec5ec9ed9886f98e0faa916a15f68f789eaef1
Aldammers <31691842+Aldammers@users.noreply.github.com>
2018-06-19 20:53:15 +0200

    Update index.js

--- Code/JS/index.js
+++ Code/JS/index.js
@@ -1,4 +1,14 @@
-/// global variables
+// global variables
+var prevValue;
+var slider;
+
+// default settings
+var currYear = '2008';
+var pieYear = '2015';
+var currCategory = 'military';
+var currCountry = 'NLD';
+
+// global variables
 var prevValue;
 var slider;
 
@@ -16,33 +26,43 @@ var categories;
 
 
 // set margin, width and height
-var margin = {top: 60, right: 40, bottom: 20, left: 50};
+var margin = {top: 50, right: 30, bottom: 30, left: 40};
 
 window.onload = function(){
 
     // read in all datasets
     d3.queue()
-        .defer(d3.json, 'mapData2.json')
-        .await(ready); 
-        
+        .defer(d3.json, 'data/mapData2.json')
+        .await(ready);
+
    // when datasets reading is ready, display data in visualisation
     function ready(error, mapData2) {
-        if (error) throw window.alert(error);
-        
-        Worldmap(mapData2, currYear, currCategory);
-        years = Object.keys(mapData2);
-        
-        slider = document.getElementById('slider-years');
-        slider.oninput = function() {
+        if (error) throw window.alert('Sorry, something is wrong with the data');
 
-            // delete map and legend
-            d3.select('.datamap').remove();
-            d3.select('.datamaps-legend').remove();
-            var newYear = slider.value;
-            
-            // draw all visualisations
-            Worldmap(mapData2, newYear, currCategory);
-        };
+
+
+        // draw all visualisations
+        drawWorldmap(mapData2, currYear, currCategory);
+ 
+        // redraw map after chosen category
+        d3.selectAll('.btn.btn-default.map')
+            .on('click', function() {
+
+                // change color of selected button and remove all visualisations
+                buttonColor(this);
+                d3.select('.datamap').remove();
+                d3.select('.datamaps-legend').remove();
+             //   d3.selectAll('.piechart').remove();
+
+                // store the selected category by the user
+                var indexCategory = this.getAttribute('value');
+                currCategory = categories[indexCategory];
+
+                // redraw all the visualizations
+                drawWorldmap(mapData2, Year, currCategory);
+
+            });
+
     };
 
     // default button color
